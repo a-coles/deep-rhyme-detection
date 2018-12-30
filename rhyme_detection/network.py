c4620002f9dcf12a917d59fc3ac00319d6bf34dd
Arlie Coles <arlie.coles@mail.mcgill.ca>
2018-12-30 16:02:53 -0500

    Restructure into classes; cleaning

--- rhyme_detection/network.py
+++ rhyme_detection/network.py
@@ -10,11 +10,9 @@ from keras.utils import to_categorical, np_utils
 from keras.optimizers import Adam
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Bidirectional
-from keras.layers.embeddings import Embedding
 
 from keras.preprocessing import sequence
 from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import one_hot
 
 from keras.callbacks import Callback, EarlyStopping, ModelCheckpoint, CSVLogger
 
@@ -22,54 +20,116 @@ from sklearn.model_selection import train_test_split
 
 from tqdm import tqdm
 
-def get_char_mapping(concat_words):
-	chars = sorted(list(set(concat_words)))
-	char_to_int = dict((c, i) for i, c in enumerate(chars))
-	# Let & be the 'between-word' tag
-	char_to_int['&'] = len(chars)
-	return char_to_int
+class Corpus:
+	def __init__(self, corpus_file):
+		self.corpus_file = corpus_file
+		self.corpus_dir = os.path.dirname(corpus_file)
+		self.language = corpus_file.split('.')[0][:-2]	# 'en', 'fr', ...
+		self.read_corpus()
 
-def get_char_to_int(char_to_int, word):
-	int_list = []
-	for char in word:
-		int_list.append(char_to_int[char])
-	return int_list
+	def read_corpus(self):
+		with open(self.corpus_file, 'r') as fp:
+			corpus_lines = fp.readlines()#[:5000]	# Debug
+			corpus_lines = [x.split(',') for x in corpus_lines]
+		self.words1 = [x[0].lower() for x in corpus_lines]
+		self.words2 = [x[1].lower() for x in corpus_lines]
+		self.labels = [x[2].lower() for x in corpus_lines]
 
-def pad_to_length(word, word_length):
-	while len(word) < word_length:
-		word += ' '
-	return word
+		self.words = list(set(self.words1 + self.words2))
+		self.concat_words = ' '.join(self.words)
+		self.word_length = max(len(max(self.words1, key=len)), len(max(self.words2, key=len)))
+		self.seq_length = (self.word_length * 2) + 1
 
-def prepare_pairs(words1, words2, char_to_int, word_length, verbose=False):
-	# Let the word length be the length of the longest string in the word lists
-	dataX = []
-	if verbose:
-		loop = tqdm(enumerate(words1))
-	else:
-		loop = enumerate(words1)
-	for i, word in loop:
-		word2 = words2[i]
-		seq = pad_to_length(word, word_length) + '&' + pad_to_length(word2, word_length)
-		int_seq = get_char_to_int(char_to_int, seq)
-		dataX.append(int_seq)
-	return dataX
+	def get_char_mapping(self):
+		'''
+		Gets a dictionary mapping each character to a unique integer.
+		'''
+		chars = sorted(list(set(self.concat_words)))
+		char_to_int = dict((c, i) for i, c in enumerate(chars))
+		char_to_int['&'] = len(chars)	# Let & be the 'between-word' tag
+		self.char_to_int = char_to_int
+		self.num_chars = len(char_to_int)
 
-def build_network_en(num_chars, input_shape, num_lstm_units):
-	embedding_vector_length = 150
-	model = Sequential()
-	model.add(Embedding(num_chars, embedding_vector_length, input_length=input_shape[0]))
-	model.add(Bidirectional(LSTM(num_lstm_units)))
-	model.add(Dense(2, activation='softmax'))
-	adam = Adam(lr=0.01)
-	model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
-	print(model.summary())
-	return model
+	def get_char_to_int(self, word):
+		int_list = []
+		for char in word:
+			int_list.append(self.char_to_int[char])
+		return int_list
 
-def train_network_en(X_train, y_train, model, num_epochs, batch_size):
-	print(batch_size)
-	callbacks = [EarlyStopping(monitor='val_loss', patience=2), ModelCheckpoint(filepath=os.path.join('..', 'models', 'rhyme_en.h5'), monitor='val_loss', save_best_only=True)]
-	model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_split=0.10, shuffle=True, callbacks=callbacks)
-	return model
+	def pad_to_length(self, word):
+		while len(word) < self.word_length:
+			word += ' '
+		return word
+
+	def prepare_pairs(self, verbose=False):
+		'''
+		Creates pairs of integer-coded padded words by character,
+		separated by a '&' integer code.
+		'''
+		dataX = []
+		if verbose:
+			loop = tqdm(enumerate(self.words1))
+		else:
+			loop = enumerate(self.words1)
+		for i, word in loop:
+			word2 = self.words2[i]
+			seq = pad_to_length(word) + '&' + pad_to_length(word2)
+			int_seq = get_char_to_int(seq)
+			dataX.append(int_seq)
+		return dataX
+
+	def get_onehot(self, dataX):
+		'''
+		Converts the integer-coded representation into a one-hot encoding.
+		'''
+		dataX = np.array([to_categorical(pad_sequences((data,), seq_length), num_chars + 1)  for data in dataX])
+		dataX = np.array([data[0] for data in dataX])
+		return dataX
+
+	def prepare_data(self, preprocessed=False):
+		if not preprocessed:
+			pairs = prepare_pairs()
+			onehot = get_onehot()
+		else:
+			with open(os.path.join(self.corpus_dir, 'rhyme_onehot_{}.pickle'.format(self.language))) as jar:
+				onehot = pickle.load(jar)
+		self.dataX = onehot
+
+
+class Network:
+	def __init__(self, corpus, num_lstm_units=8, num_epochs=10, learning_rate=0.01, batch_size=2048):
+		self.corpus = corpus
+		self.model_dir = os.path.join(corpus.corpus_dir, '..', 'models')
+
+		self.num_lstm_units = num_lstm_units
+		self.num_epochs = num_epochs
+		self.learning_rate = learning_rate
+		self.batch_size = batch_size
+
+		self.train_test_split()
+
+	def train_test_split(self):
+		self.X_train, self.X_test, y_train, y_test = train_test_split(corpus.dataX, corpus.labels, test_size=0.2, random_state=42)
+		self.y_train = to_categorical(y_train)
+		self.y_test = to_categorical(y_test)
+		self.input_shape = X_train[0].shape
+
+	def build_network_en(self):
+		model = Sequential()
+		model.add(Bidirectional(LSTM(num_lstm_units), input_shape=input_shape))
+		model.add(Dense(2, activation='softmax'))
+		adam = Adam(lr=self.learning_rate)
+		model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+		print(model.summary())
+		self.model = model
+
+	def train_network_en(self):
+		callbacks = [EarlyStopping(monitor='val_loss', patience=2), 
+					 ModelCheckpoint(filepath=os.path.join(self.model_dir, 'rhyme_en.h5'), 
+					 monitor='val_loss', save_best_only=True)]
+		self.model.fit(self.X_train, self.y_train, 
+					   batch_size=self.batch_size, epochs=self.num_epochs, 
+					   validation_split=0.10, shuffle=True, callbacks=callbacks)
 
 
 
@@ -79,59 +139,15 @@ if __name__ == '__main__':
 	parser.add_argument('--preprocessed', help='Loads a prestored pickle of the data.', action="store_true")
 	args = parser.parse_args()
 
+	# Set up the corpus object
 	corpus_file = os.path.join('..', 'corpora', 'rhyme_corpus_en.txt')
-	with open(corpus_file, 'r') as fp:
-		corpus_lines = fp.readlines()#[:5000]	# Debug
-		corpus_lines = [x.split(',') for x in corpus_lines]
-	words1 = [x[0].lower() for x in corpus_lines]
-	words2 = [x[1].lower() for x in corpus_lines]
-	labels = [x[2].lower() for x in corpus_lines]
-	words = list(set(words1 + words2))
-	concat_words = ' '.join(words)
-	word_length = max(len(max(words1, key=len)), len(max(words2, key=len)))
-	seq_length = (word_length * 2) + 1
-
-	if args.preprocessed:
-		if args.language == 'english':
-			with open(os.path.join('..', 'corpora', 'rhyme_en.pickle'), 'rb') as jar:
-				dataX = pickle.load(jar)
-			with open(os.path.join('..', 'models', 'char_to_int_en.pickle'), 'rb') as jar:
-				char_to_int = pickle.load(jar)
-			num_chars = len(char_to_int)
-			with open(os.path.join('..', 'models', 'word_length_en.pickle'), 'rb') as jar:
-				word_length = pickle.load(jar)
-	else:
-		# Get character-to-int mapping
-		char_to_int = get_char_mapping(concat_words)
-		num_chars = len(char_to_int)
-		with open(os.path.join('..', 'models', 'char_to_int_en.pickle'), 'wb') as jar:
-			pickle.dump(char_to_int, jar, protocol=2)
-		with open(os.path.join('..', 'models', 'word_length_en.pickle'), 'wb') as jar:
-			pickle.dump(word_length, jar, protocol=2)
-
-		# Get word pairs
-		dataX = prepare_pairs(words1, words2, char_to_int, word_length)
-		print('Integer encoding:')
-		dataX = np.array(dataX)
-		print(dataX)
-		with open(os.path.join('..', 'corpora', 'rhyme_en.pickle'), 'wb') as jar:
-			pickle.dump(dataX, jar, protocol=2)
-
-	# Split into train/test
-	print('Splitting into train/test.')
-	X_train, X_test, y_train, y_test = train_test_split(dataX, labels, test_size=0.2, random_state=42)
-	y_train = to_categorical(y_train)
-	y_test = to_categorical(y_test)
-	input_shape = X_train[0].shape
-	print(X_train[0])
-	print(X_train.shape)
+	corpus = Corpus(corpus_file)
+	corpus.prepare_data(preprocessed=args.preprocessed)
 
 	# Build and train network
-	num_lstm_units = 8
-	model = build_network_en(num_chars, input_shape, num_lstm_units)
-	num_epochs = 10
-	batch_size = 2048
-	model = train_network_en(X_train, y_train, model, num_epochs, batch_size)
+	network = Network(corpus)
+	network.build_network_en()
+	network.train_network_en()
 
 
 	
--- rhyme_detection/rhyme.py
+++ rhyme_detection/rhyme.py
@@ -1,29 +1,98 @@
 '''Functions for getting rhyme scheme'''
 
-from keras.models import load_model
-from network import prepare_pairs
-
 import argparse
 import os
 import pickle
 import numpy as np
+import string
 
-def does_rhyme(word1, word2, char_to_int, word_length, model):
-	# Prepare a pair for the two words
-	pair = prepare_pairs([word1], [word2], char_to_int, word_length)
-	pair = np.array(pair[0])
+from keras.models import load_model
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
 
-	# Predict whether the pair rhymes.
-	# Since the model was trained on a minibatch, we need to still
-	# respect that dimension, even though we're just going to predict one point.
-	pair = np.expand_dims(pair, axis=0)
-	probs = model.predict(pair)[0]
-	if probs[1] >= probs[0]:
-		pair_rhymes = True
-	else:
-		pair_rhymes = False
-	
-	return pair_rhymes
+from network import Corpus, Network
+
+
+class Rhymescheme:
+	def __init__(self, text_lines, corpus, model):
+		self.text_lines = text_lines
+		self.corpus = corpus
+		self.model = model
+		self.load_text()
+
+	def load_text(self):
+		self.orig_words = [word for line in self.text_lines for word in line.split(' ')]
+		self.clean_words = [word.strip().strip(string.punctuation).lower() for word in self.orig_words]
+
+	def get_char_to_int(self, word):
+		int_list = [corpus.char_to_int[char] for char in word]
+		return int_list
+
+	def pad_to_length(self, word):
+		while len(word) < corpus.word_length:
+			word += ' '
+		return word
+
+	def prepare_pair(self, word1, word2):
+		seq = self.pad_to_length(word1) + '&' + self.pad_to_length(word2)
+		int_seq = self.get_char_to_int(seq)
+		return int_seq
+
+	def get_onehot(self, seq):
+		seq = [seq]
+		onehot = np.array([to_categorical(pad_sequences((data,), corpus.seq_length), corpus.num_chars+1)  for data in seq])
+		onehot = np.array([data[0] for data in onehot])
+		return onehot
+
+	def does_rhyme(self, word1, word2):
+		'''
+		Predicts whether a pair of words rhymes.
+		'''
+		pair = self.prepare_pair(word1, word2)
+		onehot = self.get_onehot(pair)
+		probs = self.model.predict(onehot)[0]
+		if probs[1] >= probs[0]:
+			pair_rhymes = True
+		else:
+			pair_rhymes = False
+		
+		return pair_rhymes
+
+	def get_rhyme_scheme(self):
+		'''
+		Heuristically determines rhyme scheme this way:
+		- For all words, look back at every word preceding it.
+		- If the word and a preceding word rhyme, code them together.
+		'''
+		rhyme_scheme = {}
+		for i, word in enumerate(self.clean_words):
+			if word in rhyme_scheme.keys():
+				# We have already assigned a rhyme to this word
+				continue
+
+			rhyme_scheme[word] = 0
+			found_rhyme = False
+			for j in range(i):
+				preceding_word = self.clean_words[j]
+				if self.does_rhyme(preceding_word, word):
+					found_rhyme = True
+					#print('{} rhymes with {}'.format(word, preceding_word))
+					#if preceding_word != word:
+					if rhyme_scheme[preceding_word] == 0:
+						num = 1
+						while num in rhyme_scheme.values():
+							num += 1
+						rhyme_scheme[preceding_word] = num
+					rhyme_scheme[word] = rhyme_scheme[preceding_word]
+					#else:
+			if not found_rhyme:
+				print('No rhyme found for {}'.format(word))
+
+		self.rhyme_scheme = rhyme_scheme
+		print(self.clean_words)
+		print(self.rhyme_scheme)
+
+
 
 
 if __name__ == '__main__':
@@ -31,15 +100,21 @@ if __name__ == '__main__':
 	parser.add_argument('language', help='Can be english.')
 	args = parser.parse_args()
 
-	# Import the model and mapping
 	if args.language == 'english':
-		model = load_model(os.path.join('..', 'models', 'rhyme_en.h5'))
-		with open(os.path.join('..', 'models', 'char_to_int_en.pickle'), 'rb') as jar:
-			char_to_int = pickle.load(jar)
-		with open(os.path.join('..', 'models', 'word_length_en.pickle'), 'rb') as jar:
-			word_length = pickle.load(jar)
+		model_file = os.path.join('..', 'models', 'rhyme_en.h5')
+		corpus_file = os.path.join('..', 'corpora', 'rhyme_corpus_en.txt')
 
-	print(does_rhyme('sing', 'king', char_to_int, word_length, model))
-	print(does_rhyme('splendor', 'render', char_to_int, word_length, model))
-	print(does_rhyme('host', 'below', char_to_int, word_length, model))
-	print(does_rhyme('red', 'blue', char_to_int, word_length, model))
+	# Import the model
+	model = load_model(model_file)
+
+	# Set up the corpus object
+	corpus = Corpus(corpus_file)
+	corpus.get_char_mapping()
+
+	# Load in the test file
+	with open(os.path.join('..', 'test_files', 'deck_thyself.txt')) as fp:
+		text_lines = fp.readlines()
+
+	# Get the rhyme scheme
+	rhyme_scheme = Rhymescheme(text_lines, corpus, model)
+	rhyme_scheme.get_rhyme_scheme()
