a516a44fe466d033d39072e53761591ed9ce3bf1
Arlie Coles <arlie.coles@mail.mcgill.ca>
2019-01-02 22:09:26 -0500

    Multiple stanza support + cleaning

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-rhyme-detection
+# deep-rhyme-detection
+
+This software is designed to detect rhyme and rhyme schemes, especially in the context of poetry or lyrics. It trains and uses a recurrent neural network in order to predict which parts of the text rhyme with each other. This repository also contains some scripts for the assembly of rhyming corpora and for rendering of the text with rhyme scheme included.
+
+#### Example
+
+| Input | Output |
+| --- | --- |
+| `My song is love unknown,` | `/My/ {song} (is) [love] <unknown,>` |
+| `my Savior's love to me;` | `/my/ #Savior's# [love to me;]` |
+| `love to the loveless shown,` | `[love to the loveless] <shown,>` |
+| `that they might lovely be.` | `!that! <they> *might* <lovely> [be.]` |
+| `O who am I, that for my sake,` | `[O who] *am I,* !that! {for} /my/ !sake,!` |
+| `my Lord should take frail flesh and die?` | `/my/ {Lord} [should] !take! #frail flesh# /and die?/` |
+
+
+#### Supported languages
+
+* English
+* French _(coming soon)_
+
+
+## Installation
+
+To install, first clone this repo:
+
+```
+cd path/to/desired/install/location
+git clone https://github.com/violetguos/project-ift6390.git
+```
+
+Then install dependencies by running:
+
+```
+pip install -r requirements.txt
+```
+
+## Use
+
+This repo is designed to be easily integratable with other projects, with functions and classes sorted into modules. To see how it works, though, you can run the top-level script in the main of `rhyme.py` like this:
+
+```
+cd deep_rhyme_detection/
+python rhyme.py [language] [input_file] [output_dir]
+```
+
+where the arguments are:
+
+* `language`, the language of the text you want to analyze. This can only currently take the value `english` (one day there will hopefully be support for other languages). 
+* `input_file`, the path to the file containing the text to analyze. This should be a simple text file where lines in a stanza are separated by line breaks and stanzas are separated by a blank line. Punctuation and the like is fine to include.
+* `output_dir`, the path to the directory where the analyzed text will be dumped to a new text file.
+
+
+
+## Other scripts
+
+### Corpus building
+
+This repo comes with a pre-created corpus of English rhyming data, built from [WordFrequency's list of most frequent English words](https://www.wordfrequency.info/free.asp) and the [Datamuse Python API](https://github.com/gmarmstrong/python-datamuse/). The corpus is constructed by querying Datamuse for words that rhyme with each frequent English word and generating pairs of rhyming and non-rhyming words from these results.
+
+If you would like to create a corpus yourself, you can do so like this:
+
+```
+cd deep_rhyme_detection/
+python corpus.py [language] [top_num]
+```
+
+where the arguments are:
+
+* `language`, the language for which you want to build a corpus. This can only currently take the value `english` (one day there will hopefully be support for other languages). 
+* `top_num`, an integer representing the number of frequent words to use, with maximum 5000. A too-large value may introduce too much noise into the data.
+
+### Network training
+
+This repo comes with a pretrained RNN that predicts whether two English words rhyme with each other, `rhyme_en.h5` under the `models/` directory. The English network could always use more tuning (pull requests welcome!), but for now, it consists of a 6-layer bidirectional character-level LSTM, each with 32 units, using an Adam optimizer and a cross-entropy loss.
+
+If you would like to re-train the RNN with different hyperparameters, you can adjust their values in the main method of `network.py`. Open the `network.py` file in a text editor and change the values in the main as you see fit:
+
+```
+# Set network parameters - change these if retraining needed
+num_lstm_units = 8
+num_epochs = 10
+learning_rate = 0.01
+batch_size = 2048
+```
+
+Then, from the command line, run:
+
+```
+python network.py [language] [--preprocessed]
+```
+
+where the arguments are:
+
+* `language`, the language of the training corpus. This can only currently take the value `english` (one day there will hopefully be support for other languages). 
+* `--preprocessed`, a switch you should only set if you have an offline `pickle` of your training corpus in the onehot format created by the `Corpus` class.
--- deep_rhyme_detection/rhyme.py
+++ deep_rhyme_detection/rhyme.py
@@ -12,10 +12,10 @@ from keras.utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
 
 from network import Corpus, Network
-from collections import OrderedDict
+from tqdm import tqdm
 
 
-class Rhymescheme:
+class Stanza:
 	def __init__(self, text_lines, corpus, model):
 		self.text_lines = text_lines
 		self.corpus = corpus
@@ -71,9 +71,9 @@ class Rhymescheme:
 		- If the word and a preceding word rhyme, code them together.
 		'''
 		rhyme_scheme = {}
-		for i, word in enumerate(self.clean_words):
+		for i, word in enumerate(tqdm(self.clean_words)):
+			# If we already found the rhyme code for the word, skip it
 			if word in rhyme_scheme.keys():
-				# We have already assigned a rhyme to this word
 				continue
 
 			rhyme_scheme[word] = 0
@@ -82,8 +82,6 @@ class Rhymescheme:
 				preceding_word = self.clean_words[j]
 				if self.does_rhyme(preceding_word, word):
 					found_rhyme = True
-					#print('{} rhymes with {}'.format(word, preceding_word))
-					#if preceding_word != word:
 					if rhyme_scheme[preceding_word] == 0:
 						num = 1
 						while num in rhyme_scheme.values():
@@ -91,11 +89,11 @@ class Rhymescheme:
 						rhyme_scheme[preceding_word] = num
 					rhyme_scheme[word] = rhyme_scheme[preceding_word]
 					#else:
-			if not found_rhyme:
-				print('No rhyme found for {}'.format(word))
+			#if not found_rhyme:
+			#	print('No rhyme found for {}'.format(word))
 
 		self.rhyme_scheme = rhyme_scheme
-		print(self.rhyme_scheme)
+		#print(self.rhyme_scheme)
 
 	def get_rhyming_blocks(self):
 		'''
@@ -142,17 +140,15 @@ class Rhymescheme:
 					combined_code = (combined_words, key)
 					rhyme_blocks.append(combined_code)
 
-		print(rhyme_blocks)
+		#print(rhyme_blocks)
 		return rhyme_blocks
 
-	def scheme_to_text(self):
-		delimiters = [['{','}'], ['[',']'], ['(',')'], ['<','>'], ['/','/'],
-					  ['`','`'], ['!','!'], ['#','#'], ['$','$'], ['%','%'],
-					  ['^','^'], ['&','&'], ['*','*'], ['~','~'], ['?','?']]
-		rhyme_blocks = self.get_rhyming_blocks()
+	def scheme_to_orig(self, rhyme_blocks):
+		'''
+		Back-maps the rhyme scheme to the original text, including formatting
+		with punctuation and line breaks.
+		'''
 		rhyme_block_words = [item[0] for item in rhyme_blocks]
-
-		# Back-map to formatted (with punctuation and line breaks) original text
 		formatted_blocks = []
 		counter = 0
 		for i, block in enumerate(rhyme_block_words):
@@ -162,24 +158,67 @@ class Rhymescheme:
 			formatted_block = (formatted_string, rhyme_blocks[i][1])
 			formatted_blocks.append(formatted_block)
 			counter += length
+		return formatted_blocks
+
+	def scheme_to_text(self, rhyme_blocks=None, stanza_num=None):
+		if stanza_num:
+			print('Calculating rhyme scheme for stanza {}...'.format(stanza_num+1))
+
+
+		delimiters = [['{','}'], ['[',']'], ['(',')'], ['<','>'], ['/','/'],
+					  ['`','`'], ['!','!'], ['#','#'], ['$','$'], ['%','%'],
+					  ['^','^'], ['&','&'], ['*','*'], ['~','~'], ['?','?']]
+		if not rhyme_blocks:
+			rhyme_blocks = self.get_rhyming_blocks()
+		rhyme_block_words = [item[0] for item in rhyme_blocks]
+
+		# Back-map to formatted (with punctuation and line breaks) original text
+		formatted_blocks = self.scheme_to_orig(rhyme_blocks)
 
 		delimited_text = []
 		for block in formatted_blocks:
 			delimiter = delimiters[block[1]]
 			delimited = delimiter[0] + block[0] + delimiter[1]
+			# Push newlines after the delimiter
 			if '\n' in delimited:
 				delimited = delimited.replace('\n', '') + '\n'
 			delimited_text.append(delimited)
 		delimited_string = ' '.join(delimited_text)
-		print(delimited_string)
+		#print(delimited_string)
 		return delimited_string
 
+class Poem:
+	'''
+	Poems have multiple stanzas. This class gives functions for handling text with many
+	independent stanzas or verses (rhyme schemes start over at the stanza).
+	'''
+	def __init__(self, full_text_lines, corpus, model):
+		self.full_text_lines = full_text_lines
+		self.corpus = corpus
+		self.model = model
+		self.get_stanzas()
+		self.rhyme_blocks = None	# To be filled by below functions as needed
 
+	def get_stanzas(self):
+		'''
+		Split the original text on empty lines, into stanzas/verses.
+		'''
+		self.stanzas_list = [list(group) for key, group in itertools.groupby(self.full_text_lines, lambda x: x == '\n') if not key]
+		self.stanzas = [Stanza(stanza, self.corpus, self.model) for stanza in self.stanzas_list]
 
+	def get_rhyme_blocks(self):
+		self.rhyme_blocks = [stanza.get_rhyming_blocks() for stanza in self.stanzas]
+
+	def get_rhyme_scheme_text(self):
+		#if not self.rhyme_blocks:
+		#	self.get_rhyme_blocks()
+		self.rhyme_scheme = [stanza.scheme_to_text(stanza_num=i) for i, stanza in enumerate(self.stanzas)]
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('language', help='Can be english.')
+	parser.add_argument('input_file', help='Path to the input file.')
+	parser.add_argument('output_path', help='Path to the output directory.')
 	args = parser.parse_args()
 
 	if args.language == 'english':
@@ -187,16 +226,33 @@ if __name__ == '__main__':
 		corpus_file = os.path.join('..', 'corpora', 'rhyme_corpus_en.txt')
 
 	# Import the model
+	print('Loading model...')
 	model = load_model(model_file)
 
 	# Set up the corpus object
+	print('Setting up corpus...')
 	corpus = Corpus(corpus_file)
 	corpus.get_char_mapping()
 
 	# Load in the test file
-	with open(os.path.join('..', 'test_files', 'deck_thyself.txt')) as fp:
+	with open(args.input_file) as fp:
 		text_lines = fp.readlines()
 
 	# Get the rhyme scheme
-	rhyme_scheme = Rhymescheme(text_lines, corpus, model)
-	rhyme_scheme.scheme_to_text()
+	print('Getting rhyme scheme.')
+	poem = Poem(text_lines, corpus, model)
+	poem.get_rhyme_scheme_text()
+	for stanza in poem.rhyme_scheme:
+		print(stanza)
+	#rhyme_scheme = Stanza(text_lines, corpus, model)
+	#rhyme_scheme.scheme_to_text()
+
+	# Save to output file
+	input_filename = os.path.basename(args.input_file)
+	input_split = input_filename.split('.')
+	output_filename = input_split[0] + '_r.' + input_split[1]
+	output_file = os.path.join(args.output_path, output_filename)
+	with open(output_file, 'w') as fp:
+		for stanza in poem.rhyme_scheme:
+			fp.write(stanza)
+			fp.write('\n')
